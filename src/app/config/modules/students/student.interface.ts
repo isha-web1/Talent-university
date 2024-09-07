4ee0c3a9317fb5c2b7e4ef33706eaff70b50106e
isha-web1 <isha.web1@gmail.com>
2024-09-07 19:06:01 +0600

    create student schema

--- src/app/config/modules/students/student.interface.ts
+++ src/app/config/modules/students/student.interface.ts
@@ -1,28 +1,38 @@
-
-
 export type Guardian = {
-    fatherName : string,
-    fatherOccupation : string,
-    fatherContactNo : string,
-    motherName : string,
-    motherOccupation : string,
-    motherContactNo : string
-}
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
 
-export type student = {
-    id : string;
-    name: {
-        firstName : string;
-        middleName : string;
-        lastName : string
-    },
-    gender : "male" |"female";
-    dateOfBirth : string;
-    email: string;
-    emergencyContactNo : string;
-    contactNumber : string;
-    bloodGroup :  'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
-    presentAddress : string;
-    permanentAddress : string;
-    guardian : Guardian
-  }
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth?: string;
+  email: string;
+  emergencyContactNo: string;
+  contactNumber: string;
+  bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
